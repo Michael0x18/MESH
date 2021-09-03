1ec12f563d6a8f3ed230ea76d144901dafcc1e74
Michael <michaelsunn101@gmail.com>
2021-09-02 17:53:00 -0700

    Complete POC. Use expr library for evaluation

--- src/eval.c
+++ src/eval.c
@@ -9,16 +9,29 @@
 #include <string.h>
 
 #include "eval.h"
+#include "../expr/expr.h"
 
 /*
  * Takes in a null terminated string and returns the result, in null terminated string form.
  * Currently just copies it.
  * TODO Fix this to actually eval stuff.
  */
-char* eval(char* s1){
-	char* s2 = malloc(strlen(s1)*sizeof(char));
-	strcpy(s2,s1);
 
+static struct expr_func user_funcs[]={
+	{NULL,NULL,NULL,0},
+};
+
+char* eval(char* s1){
+	static char s2[1024];
+	//strcpy(s2,s1);
+	struct expr_var_list vars = {0};
+	struct expr *e = expr_create(s1, strlen(s1), &vars, user_funcs);
+	if (e == NULL) {
+    		sprintf(s2,"Syntax error");
+    		return s2;
+  	}
+	float result = expr_eval(e);
+	sprintf(s2,"%f",result);
 	return s2;
 }
 
